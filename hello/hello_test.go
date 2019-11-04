a3e51aca376d8ab04151ecd00790ca11e92f5074
Troy Connor <troy0820@gmail.com>
2019-11-04 10:52:08 -0500

    [hello]Add fmt statement to test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,13 +1,14 @@
 package hello_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/troy0820/github-actions-presentation/hello"
 )
 
 func TestHello(t *testing.T) {
-
+	fmt.Println("Starting test")
 	if want, got := "Hello World", hello.SayHello(); want != got {
 		t.Errorf("We wanted %v but got %v", want, got)
 	}
