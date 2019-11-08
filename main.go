365dce6fc891a9e47376029e684338b3b3c20a62
Troy Connor <troy0820@gmail.com>
2019-11-08 18:09:47 -0500

    [main]Changed main.go

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"bufio"
+	"flag"
 
 	"github.com/troy0820/github-actions-presentation/hello"
 )
 
 func main() {
-	fmt.Println(hello.SayHello("Troy's friends who are listening to his talk"))
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter a string to say hello: ")
+	reader.Scan()
+	var greeting string
+	fmt.Sscanf(reader.Text(), "%s", &greeting)
+	flag.Parse()
+	fmt.Println(hello.SayHello(greeting))
 }
