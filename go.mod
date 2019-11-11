5c975439d8f2c3eb26f3c504342d13741330a0be
Troy Connor <troy0820@gmail.com>
2019-11-11 17:20:29 -0500

    [main]Add writer to execute say.Hello()

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/troy0820/github-actions-presentation
 
 go 1.12
-
-require github.com/stretchr/testify v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/troy0820/github-actions-presentation/hello"
 )
 
 func main() {
 	fmt.Println(hello.SayHello("Troy's friends who are listening to his talk"))
+	fmt.Fprintf(os.Stdout, hello.SayHello("from the stdout \n"))
 }
